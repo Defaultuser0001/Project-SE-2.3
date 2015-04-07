2b7b17a6a0820e901d6cbb00fff186c1b1a13d67
Jord de Jong <jord.dejong@gmail.com>
2015-04-07 16:19:09 +0200

    Start for Gameview and board logics

--- framework/src/controller/BoardController.java
+++ framework/src/controller/BoardController.java
@@ -1,8 +1,28 @@
 package controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import model.BoardModel;
+
+import view.BoardTile;
 import view.GameBoard;
 
-public abstract class BoardController {
+public class BoardController implements ActionListener{
 
 	private GameBoard board;
+	private BoardModel model;
+	private String gameMode;
+	
+	public BoardController(int x, int y, String gameMode){
+		board = new GameBoard(x, y, gameMode, this);
+		model = new BoardModel(x, y);
+		this.gameMode = gameMode;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		BoardTile tile = (BoardTile) e.getSource();
+		// TODO: process action further...
+	}
 }
--- framework/src/view/GameBoard.java
+++ framework/src/view/GameBoard.java
@@ -2,14 +2,26 @@ package view;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public abstract class GameBoard extends JPanel{
+public class GameBoard extends JPanel{
 
     private String gameMode;
+    private ArrayList<BoardTile> tiles = new ArrayList<BoardTile>();
 
-    public GameBoard(int x, int y, String gameMode){
+    public GameBoard(int x, int y, String gameMode, ActionListener listener){
         this.setLayout(new GridLayout(x,y));
         this.gameMode = gameMode;
+        
+        for (int i = 0; i < x; i++){
+        	for (int j = 0; j < y; j++){
+        		BoardTile tile = new BoardTile(i,j);
+        		tile.addActionListener(listener);
+        		tiles.add(tile);
+        		add(tile);
+        	}
+        }
     }
 
 }
